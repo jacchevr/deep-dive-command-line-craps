833d418c060b4d84a653c19f90e8e5ea337b7f19
jacchevr <jacchevr@gmail.com>
2018-02-02 13:13:38 -0700

    Prepare to port Craps model class to other environments.

--- src/edu/cnm/deepdive/Play.java
+++ src/edu/cnm/deepdive/Play.java
@@ -4,23 +4,32 @@ import java.util.Scanner;
 
 public class Play {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
+    int autoPlays = (args.length > 0) ? Integer.parseInt(args[0]) : 0;
     Craps craps = new Craps();
+    Scanner scanner = new Scanner(System.in);
+    int wins = 0;
+    int plays = 0;
     do {
-      craps.play();
-      do {
-        int diceSum = craps.roll();
-        System.out.printf("Rolls = %d%n", diceSum);
-      } while (craps.getState() != Craps.State.WIN && craps.getState() != Craps.State.LOSS);
-      System.out.printf("Result = %s. Total plays = %d. Total wins = %d.%n",
-          craps.getState().toString(),craps.getPlays(), craps.getWins());
-      System.out.println("Play Again? (Y/N):");
-      Scanner scanner = new Scanner(System.in);
-      String input = scanner.nextLine();
-      char firstChar = input.toLowerCase().charAt(0);
-      if (firstChar != 'y') {
-        break;
+      plays++;
+      Craps.State result = craps.play();
+      if (craps.play() == Craps.State.WIN) {
+        wins++;
       }
+      if (plays >= autoPlays) {
+        System.out.println(result);
+        System.out.printf("Plays: % d. Wins: %d. Winning percentage: %.2f%%%n",
+            plays, wins, 100.0 * wins / plays);
+        for (int[] dice : craps.getRolls()) {
+          System.out.printf("%d %d%n", dice[0], dice[1]);
+        }
+        System.out.println("Play again? [Y/n]");
+        String input = scanner.nextLine().trim().toLowerCase();
+        if (!input.isEmpty() && input.charAt(0) == 'n') {
+          break;
+        }
+      }
+
     } while (true);
   }
 
